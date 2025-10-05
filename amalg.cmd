bd2816192b4a800c584edd770792bba9ec4a13c3
jamedevofficial <jame.bayerl@gmai.com>
2025-10-05 14:28:29 +0200

    added demo website

--- amalg.cmd
+++
@@ -1,8 +0,0 @@
-::Execute this script with amalg.lua installed to bundle all modules into a single .lua file
-::make sure the bin folder exists
-@echo off
-setlocal enabledelayedexpansion
-
-set params=
-for /f "delims=" %%i in (src/modules.txt) do set params=!params! %%i
-amalg.lua -s src/main.lua -o bin/bundle.lua !params!
